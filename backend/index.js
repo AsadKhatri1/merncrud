b3d5262bd660452b2b525a644dc39d4564a43ee4
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-01-06 17:35:16 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port = 5000;
 app.use(express.json());
 app.use(cors(
   {
-    origin: [""],
+    origin: ["https://merncrud-sage.vercel.app/"],
     methods:["POST","GET","PUT","DELETE"],
     credentials:true
   }
